294885171fd392fe1680f1a93e36a66116a8cd68
JoodinClooney <59059273+JoodinClooney@users.noreply.github.com>
2022-03-04 08:29:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,8 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+#
+# 添加额外软件包
+svn co https://github.com/jerrykuku/lua-maxminddb/trunk package/lua-maxminddb
+svn co https://github.com/jerrykuku/luci-app-vssr/trunk package/luci-app-vssr
+#
 # Modify default IP
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git 
 sed -i 's/192.168.1.1/192.168.10.5/g' package/base-files/files/bin/config_generate
